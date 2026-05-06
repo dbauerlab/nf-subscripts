3e77043c071381e0e86d6c1ad8671012815ee93c
James Boot <44063127+jamesboot@users.noreply.github.com>
2026-05-06 11:23:23 +0100

    Update sars-cov2_artic_illumina_vgp.sh
    Remove msa, add custom config to help memory issues, add viral_gff for snpeff process

--- vgp/sars-cov2_artic_illumina_vgp.sh
+++ vgp/sars-cov2_artic_illumina_vgp.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Script for running the viral genomics pipeline for SARS-CoV-2 ARTIC illumina sequencing data
-
 #SBATCH --partition=ncpu
 #SBATCH --job-name=vgp-artic
 #SBATCH --mem=4G
@@ -14,21 +12,22 @@ ml purge
 ml Nextflow/25.10.0
 ml Singularity/3.6.4
 
-export NXF_SINGULARITY_CACHEDIR="/nemo/stp/babs/working/bootj/nextflowcache/.singularity"
+export NXF_SINGULARITY_CACHEDIR="/nemo/lab/bauerd/home/shared/singularity"
 
-nextflow run /nemo/lab/bauerd/home/shared/github/crick-viral-genomics \
+nextflow run /nemo/lab/bauerd/home/shared/github/dev/chandra \
     -resume \
     -profile crick,nemo \
-    --samplesheet ./sub_samplesheet.csv \
+    --samplesheet ./all_artic_samplesheet.csv \
     --viral_fasta /nemo/stp/babs/working/bootj/projects/bauerd/jamie.barrett-rodger/b830/easyseq_covid19/db/artic-ncov2019/primer_schemes/nCoV-2019/V4/SARS-CoV-2.reference.fasta \
     --primers_bed /nemo/stp/babs/working/bootj/projects/bauerd/jamie.barrett-rodger/b830/easyseq_covid19/db/artic-ncov2019/primer_schemes/nCoV-2019/V4/SARS-CoV-2.primer.bed \
+    --viral_gff /nemo/stp/babs/working/bootj/projects/bauerd/jamie.barrett-rodger/b830/easyseq_covid19/db/data/NC_045512.2/genes_custom.gff \
     --run_illumina_qc_trim \
     --run_bwa_align \
     --run_artic_primer_trim \
     --run_illumina_varcall \
-    --run_msa \
     --run_panglolin \
     --run_nextclade \
     --nextclade_dataset_name "sars-cov-2" \
-    --pool_primer_reads
+    --pool_primer_reads \
+    -c myconfig.config
     
